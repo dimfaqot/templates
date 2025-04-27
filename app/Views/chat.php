6f78e01478e24d64d8fc78ee7727a2b648f943e8
dimfaqot <dimyfaqot@gmail.com>
2025-04-28 01:24:26 +0700

    webSocket

--- app/Views/chat.php
+++ app/Views/chat.php
@@ -19,7 +19,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
     <script>
-        const socket = new WebSocket("wss://192.168.1.34:8080/");
+        // const socket = new WebSocket("wss://192.168.1.34:8080/");
+        const socket = new WebSocket("wss://templates.walisongosragen.com:8080/");
 
         socket.onmessage = function(event) {
             try {
